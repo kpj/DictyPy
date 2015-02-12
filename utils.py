137cf07675bbe1d80e7c2482975f8c752efdc3d0
kpj <kpjkpjkpjkpjkpjkpj@gmail.com>
2015-02-12 22:17:05 +0100

    Fixed typo

--- utils.py
+++ utils.py
@@ -7,7 +7,7 @@ class BaseFilter(object):
         pass
 
     def apply(self):
-        """ Return True is record should be kept, False if record should be discarted
+        """ Return True if record should be kept, False if record should be discarted
         """
         raise Exception('Filter apply method not implemented')
 
