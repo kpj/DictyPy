c8abf68d7588070221cfeaf81b3e954f45f735a7
kpj <kpjkpjkpjkpjkpjkpj@gmail.com>
2015-02-14 19:43:18 +0100

    Added test for gene name extraction

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,5 +1,8 @@
 from unittest import TestCase
 
+from Bio.Seq import Seq
+from Bio.SeqRecord import SeqRecord
+
 import utils
 
 
@@ -15,3 +18,8 @@ class TestMethods(TestCase):
     def test_cma(self):
         self.assertEqual(utils.next_cma(4, 1, 2), 3)
         self.assertEqual(utils.next_cma(25, 4, 2.5), 7)
+
+    def test_gene_name_extraction(self):
+        record = SeqRecord(Seq('AGTC'), description='DDB0191165|DDB_G0267380 |DNA coding sequence|gene: argE on chromosome: 1 position 414980 to 416538')
+
+        self.assertEqual(utils.extract_gene_name(record), 'argE')
