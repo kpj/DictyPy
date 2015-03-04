c31e58cb694bf999f45d5c63f1b5f48f504a39be
kpj <kpjkpjkpjkpjkpjkpj@gmail.com>
2015-03-04 13:44:21 +0100

    Added explicit values to bar plot

--- plotter.R
+++ plotter.R
@@ -19,6 +19,7 @@ df$group <- factor(df$group, levels=df$group, ordered=TRUE)
 
 melted.df <- melt(df, id.var="group")
 ggplot(melted.df, aes(x=group, y=value, fill=variable)) +
-  theme(axis.text.x=element_text(angle=90, hjust=1, vjust=0)) +
   geom_bar(stat="identity", width=.5) +
+  geom_text(aes(label=ifelse(variable == "AAA", round(value, 2), ""), vjust=-0.25), size=4) +
+  theme(axis.text.x=element_text(angle=90, hjust=1, vjust=0)) +
   ggsave(filename="codon_usage.png")
