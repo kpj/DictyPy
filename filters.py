41fe9db387a052a79067f7534b96373bca2bcf06
kpj <kpjkpjkpjkpjkpjkpj@gmail.com>
2015-03-16 22:41:46 +0100

    Added more filters

--- filters.py
+++ filters.py
@@ -3,13 +3,12 @@ import json
 from Bio.SeqUtils import CodonUsage
 
 from sequence_analyzer import DNAAnalyzer
-from utils import BaseFilter
+from utils import BaseFilter, extract_gene_name
 
 
 class LengthFilter(BaseFilter):
     """ Skip all sequences which are shorter than specified threshold
     """
-
     def __init__(self):
         self.threshold = 250
 
@@ -28,7 +27,23 @@ class MultipleStopCodonFilter(BaseFilter):
         cods = [record.seq[i:i+3] for i in range(0, len(record.seq), 3)]
         return sum([cods.count(cod) for cod in self.stop_codons]) <= 1
 
+class AnnotatedGeneFilter(BaseFilter):
+    """ Skip all genes which are not annotated
+    """
+    def apply(self, record):
+        name = extract_gene_name(record)
+        return not name.startswith('DDB_')
+
+class RTEFilter(BaseFilter):
+    """ Skip Retroelements
+    """
+    def apply(self, record):
+        name = extract_gene_name(record)
+        return not name.endswith('_RTE')
+
 class LysineAbundanceFilter(BaseFilter):
+    """ Only allow gene which code for more than 76.6 lysines if scaled to a length of 1000 bases
+    """
     def __init__(self):
         self.dnaa = DNAAnalyzer(strict=False)
 
