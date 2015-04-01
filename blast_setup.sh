7e967f35165c7626bc9c0c3864903f67f3577d5d
kpj <kpjkpjkpjkpjkpjkpj@gmail.com>
2015-04-01 16:25:00 +0200

    Added blast install script

---
+++ blast_setup.sh
@@ -0,0 +1,6 @@
+wget ftp://ftp.ncbi.nlm.nih.gov/blast/executables/blast+/LATEST/ncbi-blast-2.2.30+-src.tar.gz
+tar xvf ncbi-blast-2.2.30+-src.tar.gz
+cd ncbi-blast-2.2.30+-src/c++/
+
+./configure
+make
