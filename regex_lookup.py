ca8c5d7a8aa4c5724b3cb07d6515fa2d35a821c9
kpj <kpjkpjkpjkpjkpjkpj@gmail.com>
2015-04-21 01:52:20 +0200

    Added fragmentation to regex-lookup

--- regex_lookup.py
+++ regex_lookup.py
@@ -10,12 +10,28 @@ from progressbar import ProgressBar
 from fasta_parser import FastaParser
 
 
+def frmt(s):
+    return s.format(x_min=10, x_max=50, y_min=2, y_max=40, z_min=12, z_max=45, N='[AGTCN]')
+
 def get_gene_name(desc):
     return re.search(r'gene: ([\w\-\./]+) ?', desc).group(1)
 
 def get_position(desc):
     match = re.search(r'(chromosome:.*)', desc)
     return match.group(1) if match else 'no position available'
+
+def get_subsequences(seq):
+    """ Now with hardcoded regular expressions
+    """
+    match_p = re.match(r'TGATG[AT][AGTCN]{0,40}([AGTCN]{10})(?:(?:TGA)|(?:CGA)|(?:TGT)|(?:TTA)|(?:TGC))[AGTCN]{2,40}T[AGTCN]{2,35}([AGTCN]{10})CTGA', seq)
+    if match_p:
+        return list(match_p.groups())
+
+    match_n = re.match(r'TCAG([AGTCN]{10})[AGTCN]{2,35}A[AGTCN]{2,4}(?:(?:TAA)|(?:ACA)|(?:TCG)|(?:TCA)|(?:GCA))([AGTCN]{10})[AGTCN]{0,40}[AT]CATCA', seq)
+    if match_n:
+        return list(match_n.groups())
+
+    return []
 
 def lookUp(patterns, data_file):
     """ Try to match given pattern in all fasta entries specified in given file
@@ -65,13 +81,14 @@ def lookUp(patterns, data_file):
             pos = get_position(record.description)
 
             for seq in seqs:
-                record = SeqRecord(
-                    Seq(seq, IUPAC.ambiguous_dna),
-                    id=record.id, name=record.name,
-                    description=record.description + ', ' + str(match.span())
-                )
+                for i, s in enumerate(get_subsequences(seq)):
+                    rec = SeqRecord(
+                        Seq(s, IUPAC.ambiguous_dna),
+                        id=record.id, name=record.name,
+                        description=record.description + '|' + str(match.span()) + '|' + ('fragment #%d' % i)
+                    )
 
-                SeqIO.write(record, fd, 'fasta')
+                    SeqIO.write(rec, fd, 'fasta')
 
 if __name__ == '__main__':
     regexprs = [
@@ -79,6 +96,6 @@ if __name__ == '__main__':
         '(TCAG{N}{{{z_min},{z_max}}}A{N}{{{y_min},{y_max}}}(?:(?:TAA)|(?:ACA)|(?:TCG)|(?:TCA)|(?:GCA)){N}{{{x_min},{x_max}}}[AT]CATCA)'
     ]
     lookUp(
-        [s.format(x_min=10, x_max=50, y_min=2, y_max=40, z_min=12, z_max=45, N='[AGTCN]') for s in regexprs],
+        [frmt(s) for s in regexprs],
         'dicty_primary_genomic'
     )
