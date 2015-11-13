4310caf1bc9744b3e9c838c106d65b253635f1b2
kpj <kpjkpjkpjkpjkpjkpj@gmail.com>
2015-11-13 21:42:48 +0100

    Add expression-grouping to codon-usage overview

--- scripts/codon_usage_overview.py
+++ scripts/codon_usage_overview.py
@@ -10,16 +10,41 @@ sys.path.insert(0, '.')
 
 from fasta_parser import FastaParser
 from sequence_analyzer import DNAAnalyzer
+from gene_expression_analysis import extract_expression_levels, group_expression_levels
 
 
-def output_data(codu, out_stream=sys.stdout):
+def output_data(group_codu, out_stream=sys.stdout):
     """ Output nicely formatted data
     """
+    def frmt(codu, head):
+        """ Format codon usage float according to header
+        """
+        if codu is None:
+            return 'none'
+        else:
+            flt = '{:.{width}f}'.format(codu, width=len(head)-2)
+            return flt
+
+    labels = sorted(group_codu.keys())
+    space = '   '
+
     for aa, codons in sorted(CodonUsage.SynonymousCodons.items()):
-        out_stream.write(aa + '\n')
+        out_stream.write(space.join([aa] + labels) + '\n')
         for c in codons:
-            out_stream.write('%s   %s\n' % (c, str(codu[c])))
+            data = space.join([c] + [frmt(group_codu[l][c], l) for l in labels])
+            out_stream.write('%s\n' % data)
         out_stream.write('\n')
+
+def get_codu_per_group(groups):
+    """ Compute codon usage per group
+    """
+    group_codu = {}
+    dnana = DNAAnalyzer(strict=False)
+
+    for label, genes in groups.items():
+        group_codu[label] = dnana.get_avg_codon_usage(genes)
+
+    return group_codu
 
 def main():
     """ Generate overview
@@ -27,16 +52,20 @@ def main():
     farser = FastaParser(sys.argv[1])
     genes = farser.parse()
 
-    dnana = DNAAnalyzer(strict=False)
-    codu = dnana.get_avg_codon_usage(genes)
+    exprs = extract_expression_levels(sys.argv[2]) if len(sys.argv) == 3 else None
+    if exprs is None:
+        groups = {'all': genes}
+    else:
+        groups = group_expression_levels(genes, exprs)
 
+    group_codu = get_codu_per_group(groups)
     with open('results/codon_usage_table.txt', 'w') as fd:
-        output_data(codu, fd)
+        output_data(group_codu, fd)
 
 
 if __name__ == '__main__':
-    if len(sys.argv) != 2:
-        print('Usage: %s <fasta file>' % sys.argv[0])
+    if not len(sys.argv) in [2, 3]:
+        print('Usage: %s <fasta file> [expr file]' % sys.argv[0])
         sys.exit(1)
 
     main()
